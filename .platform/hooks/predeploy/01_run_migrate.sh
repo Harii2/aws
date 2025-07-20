204b6a60b1d93f33be1cee51e50bcd0576755ffe
Hari <harinarayanmaddikari@gmail.com>
2025-07-21 02:39:00 +0530

    set run migration file.

--- .platform/hooks/predeploy/01_run_migrate.sh
+++ .platform/hooks/predeploy/01_run_migrate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "[HOOK] Running Django migrations inside container..."
-python manage.py migrate --noinput
+
+# Dynamically get container ID (Elastic Beanstalk default is usually the only container running)
+CONTAINER_ID=$(docker ps -q)
+
+echo "Running migrate inside container $CONTAINER_ID"
+
+docker exec "$CONTAINER_ID" python manage.py migrate --noinput
